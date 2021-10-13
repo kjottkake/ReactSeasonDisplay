7631595708dfd4a66faedab08bf465574a901a09
Jason <jimanproductions@gmail.com>
2021-10-13 21:02:02 +0200

    refactored app function to app class

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), //success callback
-        (err) => console.log(err) //failure callback
-    );
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position), //success callback
+//         (err) => console.log(err) //failure callback
+//     );
 
-    return (
-        <div>
-            <div>Hi there!</div>
-            <SeasonDisplay />
-        </div>
-    );
+//     return (
+//         <div>
+//             <div>Hi there!</div>
+//             <SeasonDisplay />
+//         </div>
+//     );
 
+// }
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), //success callback
+            (err) => console.log(err) //failure callback
+        );
+
+        return <div>Latitude: </div>;
+    }
 }
 
 ReactDOM.render(
