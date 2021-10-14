c29cb35cf7cd83f1e1265c71117d58b79087a32f
Jason <jimanproductions@gmail.com>
2021-10-14 10:32:24 +0200

    removed commented code

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-// const App = () => {
-//     window.navigator.geolocation.getCurrentPosition(
-//         (position) => console.log(position), //success callback
-//         (err) => console.log(err) //failure callback
-//     );
-
-//     return (
-//         <div>
-//             <div>Hi there!</div>
-//             <SeasonDisplay />
-//         </div>
-//     );
-
-// }
-
 class App extends React.Component {
     render() {
         window.navigator.geolocation.getCurrentPosition(
