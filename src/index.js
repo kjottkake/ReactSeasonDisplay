43dc527b9ab3274d75e19ccdbdf143ba34715c59
Jason <jimanproductions@gmail.com>
2021-10-15 21:06:09 +0200

    updated setState for error message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
     constructor(props){
@@ -8,7 +8,7 @@ class App extends React.Component {
 
         // this is the ONLY TIME we do direct assignment
         // to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         // window.navigator.geolocation.getCurrentPosition(
         //     (position) => console.log(position), //success callback
@@ -19,15 +19,21 @@ class App extends React.Component {
             (position) => {
                 this.setState({ lat: position.coords.latitude});
             }, //success callback
-            (err) => console.log(err) //failure callback
+            (err) => {
+                this.setState({ errorMessage: err.message});
+            }
         );
     }
 
 
     // React says we have to define render!!
     render() {
-    
-        return <div>Latitude: {this.state.lat}</div>;
+        return (
+        <div>
+            Latitude: {this.state.lat}
+            Error: {this.state.errorMessage}
+        </div>
+        );
     }
 }
 
