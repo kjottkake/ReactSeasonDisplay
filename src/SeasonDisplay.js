021066d11f13628985a9e949eaae84c6839e0cd1
Jason <jimanproductions@gmail.com>
2021-10-16 12:54:55 +0200

    added get season functionN

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+// const month = new Date().getMonth();
+const month = 9;
+
+const getSeason = (lat, month) => {
+    if (lat > 0)
+    {
+        console.log("You are in the northern hemisphere.");
+        if (month > 5 && month < 8){
+            console.log("It is summer.");
+        } else {
+            console.log("It is winter.");
+        }
+    } else {
+        console.log("You are in the southern hemisphere.");
+        if (month > 5 && month < 8){
+            console.log("It is winter.");
+        } else {
+            console.log("It is summer");
+        }
+    }
+}
+
 const SeasonDisplay = (prop) => {
-    return <div>Your Latitude: {prop.lat}</div>;
+    getSeason(prop.lat, month);
+
+    return <div>
+        Your Latitude: {prop.lat}
+        </div>;
 }
 
 export default SeasonDisplay;
