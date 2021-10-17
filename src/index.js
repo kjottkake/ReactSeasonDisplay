4ad6754cdddab0c007ff40dda645791dd6949b59
Jason <jimanproductions@gmail.com>
2021-10-17 13:17:26 +0200

    created spinner component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -29,7 +30,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat = {this.state.lat}/> //we are getting a property from a state and passing it as a prop.
         }
-        return <div>Getting your location..</div>;
+        return <Spinner />;
     }
 }
 
